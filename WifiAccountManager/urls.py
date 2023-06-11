beca06aa9a0141b7008cd2f6c0b1afeff6159e5c
alpha34727 <alpha34727@gmail.com>
2023-06-11 15:14:36 +0800

    error template

--- WifiAccountManager/urls.py
+++ WifiAccountManager/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# from django.urls import re_path
+# from django.views.static import serve
+# from .settings import STATIC_ROOT
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': STATIC_ROOT}), 
 ]
