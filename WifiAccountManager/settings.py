e1a97c164f4b006594f7a89acd88a5f0aad50f00
alpha34727 <alpha34727@gmail.com>
2023-06-16 23:58:07 +0800

    homepage when not login

--- WifiAccountManager/settings.py
+++ WifiAccountManager/settings.py
@@ -145,6 +145,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 # LOGGING = {
 #     'version': 1,
 #     'disable_existing_loggers': False,
